aaf59ea2ba70ec3a99eac27d5dc8d1c79aabbbb8
Adrien Quacchia <777adrien@gmail.com>
2024-12-17 15:09:24 +0100

    fix: file typo uppercase

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+
+class FileUploader {
+    public function __construct(
+        private string $targetDirectory,
+        private SluggerInterface $slugger,
+        private LoggerInterface  $logger,
+    ) {
+    }
+     
+    public function upload(UploadedFile $file, $folder = '/uploads'): string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = strtolower($this->slugger->slug($originalFilename));
+        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+    
+        try {
+            $file->move($this->targetDirectory . $folder, $fileName);
+        } catch (FileException $e) {
+            $this->logger->error('An error occurred while uploading the file: '.$e->getMessage());
+        }
+    
+        return $folder . '/' . $fileName;
+    }
+}
